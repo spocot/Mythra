b2e6bd751fb85bfa3c4e6019eacca1f04324547f
unknown <compsci@A209-21.HS.HWRSD.LAN>
2013-03-20 12:24:37 -0400

    Added two new blocks one collideable and one noncollideable

---
+++ Mythra/src/com/github/spocot/mythra/BlockGrass.java
@@ -0,0 +1,10 @@
+package com.github.spocot.mythra;
+
+import java.awt.Color;
+
+public class BlockGrass extends Block{
+
+	public BlockGrass(int x, int y) {
+		super(false, Color.green, x, y);
+	}
+}
---
+++ Mythra/src/com/github/spocot/mythra/BlockWall.java
@@ -0,0 +1,10 @@
+package com.github.spocot.mythra;
+
+import java.awt.Color;
+
+public class BlockWall extends Block{
+
+	public BlockWall(int x, int y) {
+		super(true, Color.gray, x, y);
+	}
+}
